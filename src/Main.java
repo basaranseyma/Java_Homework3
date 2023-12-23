4ac4ffb0e9b32b2ca613d185c14adb6bdf12478c
Seyma Basaran <basaranseyma@gmail.com>
2023-12-23 17:26:37 +0300

    added code to validate triangle lengths

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,15 @@ public class Main {
         System.out.print("Enter the length of the third side of the triangle: ");
         c = inp.nextInt();
 
-        // calculate the triangle's area and inform the user
-        s = (a + b + c) / 2;
-        A = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-        System.out.println("The area of the triangle: " + A);
+        // checked if the lengths form a triangle
+        if (a > 0 && b > 0 && c > 0) { // side lengths should be greater than zero
+            if ((a + b) > c && (a + c) > b && (b + c) > a) { // the sum of any two sides should be greater than the third side
+                // calculate the triangle's area
+                s = (a + b + c) / 2;
+                A = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+                System.out.println("The area of the triangle: " + A);
+                } else System.out.println("A triangle cannot be formed with these side lengths.");
+            } else { System.out.println("Side lengths should be greater than zero.");
+        }
     }
 }
